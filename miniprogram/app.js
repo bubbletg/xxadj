8f6e75b4fa72e1638dfd8fc4f50543b57df5025d
BubbleTg <bubble.tiangui@gmail.com>
2019-05-08 14:46:57 +0800

    :boom: 删除在全局执行云函数

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -9,37 +9,16 @@ App({
       wx.cloud.init({
         traceUser: true,
       })
-      //执行云涵数，获得openid作为id
-      wx.cloud.callFunction({
-        name: 'login',
-        complete: (res) => {
-          /**
-           *  在前面写是防止res.result.openid 出错，
-           *  后面不执行了，然后实现为openid_ 赋值空
-           */
-          this.globalDataOpenid = {
-            openid_: openid_,
-          }
-          console.log("---------------quanju", res.result.openid);
-          openid_ = res.result.openid;
-          /**
-           * 设置全局变量
-           * 在后面写是防止res.result.openid 没有出错，
-           * 然后赋值获取到的openid
-           */
-          this.globalDataOpenid = {
-            openid_: openid_,
-          }
-        }
-      })
     }
-
     /**
      * 设置全局变量
      */
     this.globalData = {
       //url: "http://localhost:8080/xxadj/",
       url: "https://xxadj.bubbletg.cn/",
+    }
+    this.globalDataOpenid = {
+      openid_: openid_,
     }
   }
 })
