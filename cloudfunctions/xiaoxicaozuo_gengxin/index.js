8057fc7d08c1b118752dfedf8f3d8464f852b07e
BubbleTg <bubble.tiangui@gmail.com>
2019-05-26 19:24:47 +0800

    :green_heart: 修改信息操作
    实现信息查看，放弃接单，完成，删除

--- cloudfunctions/xiaoxicaozuo_gengxin/index.js
+++ cloudfunctions/xiaoxicaozuo_gengxin/index.js
@@ -7,13 +7,28 @@ const db = cloud.database()
 const _ = db.command
 exports.main = async (event, context) => {  
   try {
-    return await db.collection("news").where({
-      _id: event._id
-    }).update({
-      data:{
-        ifdakai:true
-      }
-    })
+    //判断是否表示已读更新，还是放弃接单更新订单表示完成
+    if(event.and == 'ifdakai'){
+      return await db.collection("news").where({
+        _id: event._id
+      }).update({
+        data:{
+          ifdakai:true
+        }
+      })
+    }else if(event.and == 'fangqijiedan'){
+      return await db.collection("daijiadingdan").where({
+        _id: event._id
+      }).update({
+        data:{
+          ifFinish:true //表示完成
+        }
+      })
+    }else if(event.and == 'shanchu'){
+      //删除操作
+      return await db.collection('news').doc(event._id).remove();
+    }
+ 
   } catch (e) {
     console.error(e)
   }
